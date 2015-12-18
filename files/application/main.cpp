9b0ae615bda1cfc695e4022a06267c6abc2e2a9a
yohanlb <yohan.brl@gmail.com>
2015-12-18 14:06:07 +0100

    Loaded and displayed a 3D model successfully

--- files/application/main.cpp
+++ files/application/main.cpp
@@ -2,9 +2,18 @@
 #include <GL/glew.h>
 #include <iostream>
 
+#include "engine/Camera.hpp"
+#include "engine/Model.hpp"
+#include "engine/Shader.hpp"
+
 int main(int argc, char** argv) {
+    
+    // Properties
+    GLuint screenWidth = 800, screenHeight = 600;
+
+
     // Initialize SDL and open a window
-    glimac::SDLWindowManager windowManager(800, 600, "GLImac");
+    glimac::SDLWindowManager windowManager(screenWidth, screenHeight, "GLImac");
 
     // Initialize glew for OpenGL3+ support
     GLenum glewInitError = glewInit();
@@ -20,6 +29,25 @@ int main(int argc, char** argv) {
      * HERE SHOULD COME THE INITIALIZATION CODE
      *********************************/
 
+
+    // Camera
+    Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+    // Initialize GLEW to setup the OpenGL Function pointers
+    glewExperimental = GL_TRUE;
+    glewInit();
+    glViewport(0, 0, screenWidth, screenHeight);
+    glEnable(GL_DEPTH_TEST);
+
+
+    // Setup and compile our shaders
+    Shader shader("shaders/shader.vs", "shaders/shader.frag");
+
+    // Load models
+    Model ourModel("../../assets/models/nanosuit/nanosuit.obj");
+
+
+
     // Application loop:
     bool done = false;
     while(!done) {
@@ -34,6 +62,30 @@ int main(int argc, char** argv) {
         /*********************************
          * HERE SHOULD COME THE RENDERING CODE
          *********************************/
+
+        // Clear the colorbuffer
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader.Use();   // <-- Don't forget this one!
+
+        // Transformation matrices
+        glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+        // Draw the loaded model
+        glm::mat4 model;
+        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f)); // It's a bit too big for our scene, so scale it down
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        
+
+        ourModel.Draw(shader);       
+
+
+
 
         // Update the display
         windowManager.swapBuffers();
---
+++ files/engine/include/engine/Mesh.hpp
@@ -0,0 +1,57 @@
+#pragma once
+// Std. Includes
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+// GL Includes
+#include <GL/glew.h> // Contains all the necessery OpenGL includes
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <assimp/types.h>
+#include "engine/Shader.hpp"
+
+
+struct Vertex {
+    // Position
+    glm::vec3 Position;
+    // Normal
+    glm::vec3 Normal;
+    // TexCoords
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    GLuint id;
+    string type;
+    aiString path;
+};
+
+class Mesh {
+
+public:
+    /*  Mesh Data  */
+    vector<Vertex> vertices;
+    vector<GLuint> indices;
+    vector<Texture> textures;
+
+    /*  Functions  */
+    // Constructor
+    Mesh(vector<Vertex> vertices, vector<GLuint> indices, vector<Texture> textures);
+
+    // Render the mesh
+    void Draw(Shader shader) ;
+
+private:
+    /*  Render data  */
+    GLuint VAO, VBO, EBO;
+
+    /*  Functions    */
+    // Initializes all the buffer objects/arrays
+    void setupMesh();
+};
+
+
+
