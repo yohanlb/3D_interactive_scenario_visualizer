90b84f9946ff59f95295879e195c429ec0081f95
AsmaeBouh <asmae.bouh@hotmail.fr>
2015-12-22 11:31:51 +0100

    skybox

--- files/application/main.cpp
+++ files/application/main.cpp
@@ -1,10 +1,45 @@
 #include <glimac/SDLWindowManager.hpp>
 #include <GL/glew.h>
 #include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "engine/Camera.hpp"
 #include "engine/Model.hpp"
 #include "engine/Shader.hpp"
+#include "glimac/Image.hpp"
+#include <string>
+#include <vector>
+
+using namespace glimac;
+using namespace std;
+
+
+
+GLuint loadCubemap(vector<string> faces)
+{
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+   std::unique_ptr<Image> image;
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+    for(GLuint i = 0; i < faces.size(); i++)
+    {
+        image = loadImage(faces[i]);
+        glTexImage2D(
+            GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0,
+            GL_RGBA, image->getWidth(), image->getHeight(), 0, GL_RGBA, GL_FLOAT, image->getPixels()
+        );
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+    return textureID;
+}  
 
 
 
@@ -40,17 +75,81 @@ int main(int argc, char** argv) {
     glewInit();
     glViewport(0, 0, screenWidth, screenHeight);
     glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
 
 
     // Setup and compile our shaders
     Shader objectsShader("shaders/multiple_lights.vs", "shaders/multiple_lights.fs");
     Shader lampObjectsShader("shaders/lamp.vs", "shaders/lamp.fs");
+    Shader skyboxShader("shaders/skybox.vs", "shaders/skybox.fs");
 
     // Load models
     Model ourModel("../../assets/models/stormtrooper/Stormtrooper.obj");
     Model lampModel("../../assets/models/moon/moon.obj");
     //Model ourModel("../../assets/models/nanosuit/nanosuit.obj");
     //Model ourModel("../../assets/models/bla.obj");
+
+
+    #pragma region "object_initialization"
+    // Set the object data (buffers, vertex attributes) 
+     
+    GLfloat skyboxVertices[] = {
+        // Positions          
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+  
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+  
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+   
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+  
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+  
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f
+    };
+
+    
+    // Setup skybox VAO
+    GLuint skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glBindVertexArray(0);
+    #pragma endregion
 
 
 
@@ -68,6 +167,16 @@ int main(int argc, char** argv) {
         glm::vec3(5.0f, 0.0f, 10.0f),
         glm::vec3(0.0f, 40.0f, 0.0f)
     };
+
+     // Cubemap (Skybox)
+    vector<string> faces;
+    faces.push_back("../../assets/textures/skybox/right.jpg");
+    faces.push_back("../../assets/textures/skybox/left.jpg");
+    faces.push_back("../../assets/textures/skybox/top.jpg");
+    faces.push_back("../../assets/textures/skybox/bottom.jpg");
+    faces.push_back("../../assets/textures/skybox/back.jpg");
+    faces.push_back("../../assets/textures/skybox/front.jpg");
+    GLuint cubemapTexture = loadCubemap(faces); 
 
 
     // Application loop:
@@ -134,6 +243,25 @@ int main(int argc, char** argv) {
         // Clear the colorbuffer
         glClearColor(0.00f, 0.00f, 0.00f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+
+         // Draw skybox first
+        glDepthMask(GL_FALSE);// Remember to turn depth writing off
+        skyboxShader.Use();     
+        glm::mat4 view1 = glm::mat4(glm::mat3(camera.GetViewMatrix()));  // Remove any translation component of the view matrix
+        glm::mat4 projection1 = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
+        glUniformMatrix4fv(glGetUniformLocation(skyboxShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view1));
+        glUniformMatrix4fv(glGetUniformLocation(skyboxShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection1));
+        // skybox cube
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glUniform1i(glGetUniformLocation(skyboxShader.Program, "skybox"), 0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthMask(GL_TRUE);
+        
+
 
         objectsShader.Use(); 
         // Transformation matrices
@@ -144,7 +272,7 @@ int main(int argc, char** argv) {
 
 
         // Set the lighting uniforms
-        glUniform3f(glGetUniformLocation(objectsShader.Program, "viewPos"), camera.Position.x, camera.Position.y, camera.Position.z);
+        //glUniform3f(glGetUniformLocation(objectsShader.Program, "viewPos"), camera.Position.x, camera.Position.y, camera.Position.z);
         // Point light 1
         glUniform3f(glGetUniformLocation(objectsShader.Program, "pointLights[0].position"), pointLightPositions[0].x, pointLightPositions[0].y, pointLightPositions[0].z);     
         glUniform3f(glGetUniformLocation(objectsShader.Program, "pointLights[0].ambient"), 0.09f, 0.09f, 0.09f);       
@@ -212,6 +340,8 @@ int main(int argc, char** argv) {
         lampObjectsShader.Use();
         glUniformMatrix4fv(glGetUniformLocation(lampObjectsShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(lampObjectsShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+         model = glm::translate(model, glm::vec3(-10.0f, 0.0f,  -5.0f));
+
         for(GLuint i = 0; i < 2; i++)
         {
             model = glm::mat4();
@@ -223,8 +353,7 @@ int main(int argc, char** argv) {
         // Move the light
         pointLightPositions[0] += glm::vec3(0.0f, 0.0f, -0.005f);
 
-
-
+        
 
         // Update the display
         windowManager.swapBuffers();
